2a209ce03b6fd1f5d6273f3a8588ed4a0930567e
Vivek Lakshmanan <vivek@pulumi.com>
2020-10-15 11:13:23 -0700

    Add basic build/deploy of image to local kind cluster

--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
+import * as docker from "@pulumi/docker";
 import * as k8s from "@pulumi/kubernetes";
-import * as kx from "@pulumi/kubernetesx";
+import * as pulumi from "@pulumi/pulumi";
 
-const appLabels = { app: "nginx" };
-const deployment = new k8s.apps.v1.Deployment("nginx", {
+const imageName = "ruby-app";
+
+const myImage = new docker.Image(imageName, {
+    imageName: pulumi.interpolate`localhost:5000/${imageName}:latest`,
+    build: {
+        context: "./app",
+    },
+});
+
+const appLabels = { app: "app" };
+const deployment = new k8s.apps.v1.Deployment("app", {
     spec: {
         selector: { matchLabels: appLabels },
         replicas: 1,
         template: {
             metadata: { labels: appLabels },
-            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+            spec: { containers: [{ name: "app", image: myImage.baseImageName }] }
         }
     }
 });
-export const name = deployment.metadata.name;
+export const deploymentName = deployment.metadata.name;
